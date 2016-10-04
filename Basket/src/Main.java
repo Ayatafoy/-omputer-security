d0ab0c6e1ea08c403373f9d5408954abb06d2d5d
RomanovAI <romanow.lesha2013@yandex.ru>
2016-10-04 14:10:05 +0300

    first test change

--- Basket/src/Main.java
+++ Basket/src/Main.java
@@ -49,7 +49,7 @@ public class Main implements Basket{
         System.out.println(prod.getProducts());
         System.out.println(prod.getProductQuantity("Клавиатура"));
         prod.updateProductQuantity("Клавиатура", 1);
-        prod.removeProduct("Мышь");
+        prod.removeProduct("Мышь гавнарская");
         System.out.println(prod.getProducts());
         System.out.println(prod.getProductQuantity("Толстый притолстый барабан"));
         prod.clear();
